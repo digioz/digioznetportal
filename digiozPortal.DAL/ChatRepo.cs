4f0a16cd05407be05a36410aa550ad15eb206a51
sartul <sartul@gmail.com>
2020-08-13 14:38:11 -0500

    new files for chat logic

---
+++ digiozPortal.DAL/ChatRepo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using digiozPortal.BO;
+using digiozPortal.DAL.Interfaces;
+using digiozPortal.Utilities;
+
+namespace digiozPortal.DAL
+{
+    public class ChatRepo : AbstractRepo<Chat>, IChatRepo 
+    {
+        public ChatRepo(IConfigHelper config) :base(config){}
+        public List<Chat> GetLatestChats(int top, string orderBy, string order, string fields="*") {
+            string sqlChats = $"SELECT TOP {top} {fields} FROM Chat ORDER BY {orderBy} {order};";
+            using (var connection = new SqlConnection(_connectionString)) {
+                return connection.Query<Chat>(sqlChats).OrderByDescending(x => x.Id).ToList();
+            }
+        }
+    }
+}
---
+++ digiozPortal.DAL/Interfaces/IChatRepo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using digiozPortal.BO;
+
+namespace digiozPortal.DAL.Interfaces
+{
+    public interface IChatRepo : IRepo<Chat>
+    {
+        List<Chat> GetLatestChats(int top, string orderBy, string order, string fields="*");
+    }
+}
