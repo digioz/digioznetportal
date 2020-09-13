6df5525bb93dc9cfefd89afc807ce8adc4f11615
Pedram Soheil <psoheil@gmail.com>
2020-09-13 16:23:45 -0500

    Admin Dashboard - Cleanup of Role Manager to remove commented out code.

--- digiozPortal.Web/Areas/Admin/Controllers/RoleManagerController.cs
+++ digiozPortal.Web/Areas/Admin/Controllers/RoleManagerController.cs
@@ -32,8 +32,6 @@ namespace digiozPortal.Web.Areas.Admin.Controllers
         }
 
         public IActionResult Index() {
-            // Identity Library does not have 
-            // _roleManager.Roles implemented;
             var models = _roleLogic.GetAll(); 
 
             return View(models);
@@ -47,21 +45,10 @@ namespace digiozPortal.Web.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create([Bind("Id", "Name")] AspNetRoles role) {
             if (ModelState.IsValid) {
-                // Option 1
                 role.Id = Guid.NewGuid().ToString();
                 role.NormalizedName = role.Name;
                 role.ConcurrencyStamp = string.Empty;
                 _roleLogic.Add(role);
-
-                // Option 2
-                //var result = await _roleManager.CreateAsync(role);
-
-                //if (result.Succeeded) {
-                //    return RedirectToAction("Index");
-                //} 
-                //else {
-                //    ViewBag.Errors = result.Errors;
-                //}
 
                 return RedirectToAction("Index");
             }
@@ -70,7 +57,6 @@ namespace digiozPortal.Web.Areas.Admin.Controllers
         }
 
         public IActionResult Delete(string id) {
-            // Option 1
             var role = _roleLogic.Get(id);
 
             if (role != null) {
@@ -78,23 +64,6 @@ namespace digiozPortal.Web.Areas.Admin.Controllers
 
                 return RedirectToAction("Index");
             }
-
-            // Option 2
-            //var role = await _roleManager.FindByIdAsync(id);
-
-            //if (role != null) {
-            //    IdentityResult result = await _roleManager.DeleteAsync(role);
-
-            //    if (result.Succeeded) {
-            //        return RedirectToAction("Index");
-            //    }
-            //    else {
-            //        ModelState.AddModelError("", result.Errors.ToString());
-            //    }
-            //} 
-            //else {
-            //    ModelState.AddModelError("", "No role found");
-            //}
                 
             return View("Index");
         }
