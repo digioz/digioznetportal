5e74c28de09f23e29b24177c8d30ae8a2c87c16b
Pedram Soheil <psoheil@gmail.com>
2021-02-23 20:42:14 -0600

    - Renamed Utilities Admin Section to Other - Coded a Log Manager Index Page with filtering to allow viewing and sorting through log entries - Added missing properties to Log class in the Bo layer (Level, Exception and LogEvent)

--- digioz.Portal/digioz.Portal.Bo/Log.cs
+++ digioz.Portal/digioz.Portal.Bo/Log.cs
@@ -9,6 +9,9 @@ namespace digioz.Portal.Bo
     {
         public int Id { get; set; }
         public string Message { get; set; }
+        public string Level { get; set; }
         public DateTime? Timestamp { get; set; }
+        public string Exception { get; set; }
+        public string LogEvent { get; set; }
     }
 }
---
+++ digioz.Portal/digioz.Portal.Web/Areas/Admin/Controllers/LogManagerController.cs
@@ -0,0 +1,52 @@
+﻿using digioz.Portal.Bll.Interfaces;
+using digioz.Portal.Bo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digioz.Portal.Web.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = "Administrator")]
+	public class LogManagerController : Controller
+	{
+        private readonly ILogic<Log> _logLogic;
+
+        public LogManagerController(
+            ILogic<Log> logLogic,
+            IConfigLogic configLogic,
+            ILogic<AspNetUser> userLogic
+        )
+        {
+            _logLogic = logLogic;
+        }
+
+        public async Task<IActionResult> Index()
+		{
+            var query = "SELECT TOP (1000) [Id],LEFT([Message],150) AS [Message], [Level], [Timestamp], '' AS [Exception], '' AS [LogEvent] FROM Log ORDER BY Id DESC;";
+            var logs = (List<Log>)_logLogic.GetQueryString(query);
+
+			return View(logs);
+		}
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var log = _logLogic.Get(id.GetValueOrDefault());
+
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            return View(log);
+        }
+    }
+}
