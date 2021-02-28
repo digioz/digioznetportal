88253127648a10c9aefaaac4fc13868ced90a203
Pedram Soheil <psoheil@gmail.com>
2021-02-28 14:08:03 -0600

    Added a Grid Table for the Admin Dashboard Landing Page to show latest visitors on the Home Index

--- digioz.Portal/digioz.Portal.Web/Areas/Admin/Controllers/HomeController.cs
+++ digioz.Portal/digioz.Portal.Web/Areas/Admin/Controllers/HomeController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using digioz.Portal.Bll.Interfaces;
+using digioz.Portal.Bo;
+using digioz.Portal.Web.Areas.Admin.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +14,24 @@ namespace digioz.Portal.Web.Areas.Admin.Controllers
     [Authorize(Roles = "Administrator")]
     public class HomeController : Controller
     {
+        private readonly ILogic<VisitorInfo> _visitorInfoLogic;
+
+        public HomeController(
+            ILogic<VisitorInfo> visitorInfoLogic
+        )
+        {
+            _visitorInfoLogic = visitorInfoLogic;
+        }
+
         public async Task<IActionResult> Index() {
-            return View();
+            var model = new HomeViewModel();
+            var query = @"SELECT TOP (1000) [Id], [IpAddress], [OperatingSystem],[Browser],[BrowserVersion], [Href], [Timestamp],
+						[JavaEnabled], [ScreenHeight], [ScreenWidth], '' AS [BrowserEngineName], '' AS [Host], 
+						'' AS [HostName], '' AS [Platform], '' AS [Referrer], '' AS [UserAgent], '' AS [UserLanguage], '' AS [SessionId]
+						FROM [VisitorInfo] ORDER BY Id DESC;";
+            model.Visitors = (List<VisitorInfo>)_visitorInfoLogic.GetQueryString(query);
+
+            return View(model);
         }
     }
 }
---
+++ digioz.Portal/digioz.Portal.Web/Areas/Admin/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using digioz.Portal.Bo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digioz.Portal.Web.Areas.Admin.Models.ViewModels
+{
+	public class HomeViewModel
+	{
+		public List<VisitorInfo> Visitors { get; set; }
+
+	}
+}
