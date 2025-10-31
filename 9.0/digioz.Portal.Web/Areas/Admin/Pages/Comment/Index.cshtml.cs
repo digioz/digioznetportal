7343924790b7d0c9d76f34e76cfb622e54c5fd43
DigiOz Multimedia <digioz@gmail.com>
2025-10-30 20:40:39 -0500

    Issue #52 -

--- 9.0/digioz.Portal.Web/Areas/Admin/Pages/Comment/Index.cshtml.cs
+++ 9.0/digioz.Portal.Web/Areas/Admin/Pages/Comment/Index.cshtml.cs
@@ -1,9 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using digioz.Portal.Dal.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace digioz.Portal.Web.Areas.Admin.Pages.Comment
 {
     public class IndexModel : PageModel
     {
-        public void OnGet() { }
+        private readonly ICommentService _service;
+        public IndexModel(ICommentService service) { _service = service; }
+
+        public IReadOnlyList<digioz.Portal.Bo.Comment> Items { get; private set; } = Array.Empty<digioz.Portal.Bo.Comment>();
+        [BindProperty(SupportsGet = true)]
+        public int PageNumber { get; set; } =1;
+        [BindProperty(SupportsGet = true)]
+        public int PageSize { get; set; } =10;
+        public int TotalCount { get; private set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / Math.Max(1, PageSize));
+
+        public void OnGet()
+        {
+            var all = _service.GetAll()
+                .OrderByDescending(c => c.ModifiedDate ?? c.CreatedDate)
+                .ToList();
+
+            TotalCount = all.Count;
+            if (PageNumber <1) PageNumber =1;
+            if (PageSize <1) PageSize =10;
+
+            var skip = (PageNumber -1) * PageSize;
+            Items = all.Skip(skip).Take(PageSize).ToList();
+        }
     }
 }
