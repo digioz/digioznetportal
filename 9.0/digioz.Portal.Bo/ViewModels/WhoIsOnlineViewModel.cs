dee75800d2d24e63f1ae2f50a7cffee965051b21
DigiOz Multimedia <digioz@gmail.com>
2025-12-01 22:20:33 -0600

    Issue #147 PR #148 - Update online user count display in WhoIsOnline
    Added a `TotalOnlineCount` property to the `WhoIsOnlineViewModel` to represent the total number of online users. Updated the `Default.cshtml` view to display `TotalOnlineCount` instead of `VisitorCount`, ensuring the view reflects the total online user count accurately.

--- 9.0/digioz.Portal.Bo/ViewModels/WhoIsOnlineViewModel.cs
+++ 9.0/digioz.Portal.Bo/ViewModels/WhoIsOnlineViewModel.cs
@@ -9,6 +9,7 @@ namespace digioz.Portal.Bo.ViewModels
     public class WhoIsOnlineViewModel
     {
         public int VisitorCount { get; set; }
+        public int TotalOnlineCount { get; set; }
         public List<VisitorSession> RegisteredVisitors { get; set; } = new List<VisitorSession>();
         public List<BotVisitorViewModel> Bots { get; set; } = new List<BotVisitorViewModel>();
         public bool WhoIsOnlineEnabled { get; set; } = false;
